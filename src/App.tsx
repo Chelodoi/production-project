ed40f0abd8de27bf58c486a01059d8f356e5a9a1
YutanovMaksim <yutanovm@yandex.ru>
2023-01-31 11:41:43 +0300

    add lazy load & routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { Suspense } from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
 import { Counter } from './components/Counter';
 import './index.scss';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPAge.async';
 
 export const App = () => {
   return (
     <div className="app">
-      A am from App Component
-      <Counter />
+      <Link to="/">Главная</Link>
+      <Link to="/about">О сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/about" element={<AboutPageAsync />} />
+          <Route path="/" element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
---
+++ src/pages/MainPage/MainPage.async.tsx
@@ -0,0 +1,9 @@
+import { lazy } from 'react';
+//Component must be export default!!!!
+export const MainPageAsync = lazy(
+  () =>
+    new Promise((resolve) => {
+      //@ts-ignore
+      setTimeout(() => resolve(import('./MainPage'), 5000));
+    })
+);
